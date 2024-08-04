6445d9c93d8ebea2963db386a675391ffb0467f8
Wemmow <wemow.2k@gmail.com>
2024-08-04 12:01:13 +0200

    Yes
    yes

---
+++ reactapp/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>Hello this is my website</p>
+      </header>
+    </div>
+  );
+}
+
+//Dagens uppgift
+//1. Skapa en section som handlar om dig (bara 1 section), (About Me)
+//2. Göra lite design för en section
+//3. Om du vill gör lite typescript för att testa.
+
+export default App;
